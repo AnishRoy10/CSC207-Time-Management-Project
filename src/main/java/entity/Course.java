0f629b71088b1dd978aa0487a823952ad1f55df0
Kojanzy <nwmarinoto@gmail.com>
2024-06-24 11:28:20 -0400

    add a `description` and `todo` field to `Course`, and expand on some docs

--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -1,21 +1,49 @@
 package entity;
 
-/**
+/*
  * The Course class represents a course that users can join.
- * This is a barebones implementation to ensure Task.java compiles.
  */
 public class Course {
+    // Name of the course
     private String name;
 
-    public Course(String name) {
+    // Description of the course
+    private String description;
+
+    // Todolist associated with this course
+    private TodoList todo;
+
+    /**
+     * Create a new Course
+     *
+     * @param name          The course name
+     * @param description   A description of the course
+     */
+    public Course(String name, String description) {
         this.name = name;
+        this.description = description;
+
+        // unsure how we will associate courses and todolists just yet
+        this.todo = new TodoList();
     }
 
+    // Getter for the course name
     public String getName() {
         return name;
     }
 
+    // Getter for the course description
+    public String getDescription() {
+        return description;
+    }
+
+    // Setter for the course name
     public void setName(String name) {
         this.name = name;
+    }
+
+    // Setter for the course description
+    public void setDescription(String description) {
+        this.description = description;
     }
 }

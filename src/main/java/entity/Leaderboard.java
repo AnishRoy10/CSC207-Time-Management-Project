40b33c459bb0cafb0aff9688a30381a0967faf17
skyy <jiayinglee0@gmail.com>
2024-06-26 00:09:02 -0400

    Added Leaderboard entity and implemented basic methods

---
+++ src/main/java/entity/Leaderboard.java
@@ -0,0 +1,44 @@
+package entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* The Leaderboard class represents a leaderboard that users can view their scores
+
+and others scores.*/
+
+public abstract class Leaderboard {
+    //Name of the leaderboard
+    private String name;
+
+    // A HashMap to store the user's scores
+
+    protected Map<String, Integer> scores;
+
+    public Leaderboard(String name) {
+        this.name = name;
+        this.scores = new HashMap<>() ;
+    }
+
+    // A method to add a user score
+    public void addScore(String username, int score) {
+        scores.put(username, score);
+    }
+
+    // An abstract method to be implemented by subclasses
+    public abstract void displayLeaderboard();
+
+
+    // A method to get the scores
+    public Map<String, Integer> getScores() {
+        return scores;
+    }
+
+    // A method to get the leaderboard's name
+    public String getName() {
+        return name;
+    }
+
+
+
+}

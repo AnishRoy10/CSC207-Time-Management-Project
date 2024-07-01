4d3ed38fec2a99fe1ffc181424eccbe5cc9c62db
Eric Xia <ercx1023@gmail.com>
2024-07-01 18:04:10 -0400

    added mongodb dependencies to pom.xml and wrote code snippets for MongoConnector

--- src/main/java/data_access/MongoConnector.java
+++ src/main/java/data_access/MongoConnector.java
@@ -1,5 +1,20 @@
 package data_access;
 
-public class MongoConnector {
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
+public class MongoConnector {
+    /**
+     * The MongoConnector class establishes a connection between the application and the MongoDB database
+     *
+     */
+    public static void main(String[] args) {
+        MongoClient mongoClient = MongoClients.create("mongodb+srv://admin1:<password>@cluster0.d61osh2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+        MongoDatabase db = mongoClient.getDatabase("Cluster0");
+
+        MongoCollection col = db.getCollection("userdata");
+    }
 }

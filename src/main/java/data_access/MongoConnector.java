be5301cde3e524139159735ea67ce53aff086e1d
Eric Xia <ercx1023@gmail.com>
2024-07-04 19:10:12 -0400

    Completed the db connector

--- src/main/java/data_access/MongoConnector.java
+++ src/main/java/data_access/MongoConnector.java
@@ -4,6 +4,8 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class MongoConnector {
     /**
@@ -13,8 +15,12 @@ public class MongoConnector {
     public static void main(String[] args) {
         MongoClient mongoClient = MongoClients.create("mongodb+srv://admin1:<password>@cluster0.d61osh2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
-        MongoDatabase db = mongoClient.getDatabase("Cluster0");
+        MongoDatabase db = mongoClient.getDatabase("TimeManagementProject");
 
-        MongoCollection col = db.getCollection("userdata");
+        MongoCollection col = db.getCollection("users");
+
+        Document sampleDoc = new Document("_id", new ObjectId("889f445ed1cb36fdc66b17ef")).append("username", "User2");
+
+        col.insertOne(sampleDoc);
     }
 }

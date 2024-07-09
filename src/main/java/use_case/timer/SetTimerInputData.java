52dba750911f0ef3ef1a8caefa3ab4fef8eacca4
Anish Roy <anishroy991@gmail.com>
2024-07-09 18:33:30 -0400

    implemented SetTimerInputData

--- src/main/java/use_case/timer/SetTimerInputData.java
+++ src/main/java/use_case/timer/SetTimerInputData.java
@@ -1,4 +1,26 @@
 package use_case.timer;
 
 public class SetTimerInputData {
+    final private int hours;
+    final private int minutes;
+    final private int seconds;
+
+    public SetTimerInputData(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
 }

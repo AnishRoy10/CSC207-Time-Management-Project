509b593e18fe3ac46d28661a711e70d038635dfd
Alex Marinescu <alexmarinescu08@gmail.com>
2024-07-11 19:39:38 -0400

    Finished CheckConflictUseCase class by adding showConflicts method and created an EventCard (like taskcard)

--- src/main/java/use_case/CheckConflictsUseCase.java
+++ src/main/java/use_case/CheckConflictsUseCase.java
@@ -16,7 +16,7 @@ public class CheckConflictsUseCase {
 
     public CheckConflictsUseCase(Calendar calendar) {this.calendar = calendar;}
 
-    // Helper function for conflictChecker that returns the events in the order of start date.
+    // Helper function for conflictChecker and showConflicts that returns the events in the order of start date.
     // If the start dates are the same, return in the order they come in.
     private ArrayList<CalendarEvent> comesFirst(CalendarEvent eventOne, CalendarEvent eventTwo) {
         if (eventTwo.getStartDate().isBefore(eventOne.getStartDate())) {
@@ -46,6 +46,25 @@ public class CheckConflictsUseCase {
         }
     }
     // Must find out what data type to use to store the information of which
-    //Events conflict for the method showConflicts
-    // public insertreturntypehere showConflicts(Calendar calendar) {}
+    // Events conflict for the method showConflicts
+    public ArrayList<ArrayList<CalendarEvent>> showConflicts(Calendar calendar) {
+        ArrayList<ArrayList<CalendarEvent>> conflictList = new ArrayList<ArrayList<CalendarEvent>>();
+        // Double loop which loop over all pairs of events in the Calendar, adding conflicting pairs to conflictList
+        for (CalendarEvent event : calendar.getAllEvents()) {
+            for (CalendarEvent anotherEvent : calendar.getAllEvents()) {
+                // Condition that the events are not the same
+                boolean conditionOne = !event.equals(anotherEvent);
+                // Condition that the two events indeed have a conflict
+                boolean conditionTwo = conflictChecker(event, anotherEvent);
+                // Condition that the events have not already been added (in order of startTime) to conflictList
+                boolean conditionThree = ! conflictList.contains(comesFirst(event, anotherEvent));
+                // If conditions are met, then the set of conflicting events are added
+                // in a list to conflictList (in order)
+                if (conditionOne && conditionTwo && conditionThree) {
+                    conflictList.add(comesFirst(event, anotherEvent));
+                }
+            }
+        }
+        return conflictList;
+    }
 }

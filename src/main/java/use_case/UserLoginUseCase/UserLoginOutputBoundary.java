de9e96e571a6ee820c023789ed26f302ca14c0a7
swagman8008 <naqeeb.ali@mail.utoronto.ca>
2024-07-15 11:33:07 -0400

    Added code for UserLoginOutputBoundary, UserLoginRequestModel and UserLoginResponseModel.

--- src/main/java/use_case/UserLoginUseCase/UserLoginOutputBoundary.java
+++ src/main/java/use_case/UserLoginUseCase/UserLoginOutputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.UserLoginUseCase;
 
-public class UserLoginOutputBoundary {
+/**
+ * Output Boundary interface for the user login use case.
+ */
+public interface UserLoginOutputBoundary {
+    /**
+     * Presents the login response.
+     *
+     * @param responseModel The response model containing the result of the login attempt.
+     */
+    void present(UserLoginResponseModel responseModel);
 }

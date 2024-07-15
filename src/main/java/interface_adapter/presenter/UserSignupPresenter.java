5add19ae1970e32a8c4260bed13866fbce58ab1c
swagman8008 <naqeeb.ali@mail.utoronto.ca>
2024-07-15 10:37:52 -0400

    Added code for the presenter and view model.

--- src/main/java/interface_adapter/presenter/UserSignupPresenter.java
+++ src/main/java/interface_adapter/presenter/UserSignupPresenter.java
@@ -1,4 +1,33 @@
 package interface_adapter.presenter;
 
-public class UserSignupPresenter {
+import use_case.UserSignupUseCase.UserSignupOutputBoundary;
+import use_case.UserSignupUseCase.UserSignupResponseModel;
+import interface_adapter.viewmodel.UserSignupViewModel;
+
+/**
+ * Presenter for handling user signup responses.
+ */
+public class UserSignupPresenter implements UserSignupOutputBoundary {
+
+    private final UserSignupViewModel userSignupViewModel;
+
+    /**
+     * Constructor for UserSignupPresenter.
+     *
+     * @param userSignupViewModel The view model for user signup.
+     */
+    public UserSignupPresenter(UserSignupViewModel userSignupViewModel) {
+        this.userSignupViewModel = userSignupViewModel;
+    }
+
+    /**
+     * Presents the signup response model to the view model.
+     *
+     * @param responseModel The response model containing the signup result.
+     */
+    @Override
+    public void present(UserSignupResponseModel responseModel) {
+        userSignupViewModel.setSignupSuccess(responseModel.isSuccess());
+        userSignupViewModel.setMessage(responseModel.getMessage());
+    }
 }

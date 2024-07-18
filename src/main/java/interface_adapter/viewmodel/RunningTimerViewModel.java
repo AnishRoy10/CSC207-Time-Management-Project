b8e773d7310a2928c9f231f33bbe29ad7d477e2f
Anish Roy <anishroy991@gmail.com>
2024-07-18 15:19:55 -0400

    started implemented the view model for the running timer

---
+++ src/main/java/interface_adapter/viewmodel/RunningTimerViewModel.java
@@ -0,0 +1,27 @@
+package interface_adapter.viewmodel;
+
+import interface_adapter.ViewModel;
+
+public class RunningTimerViewModel extends ViewModel {
+
+    public static final String TITLE_LABEL = "Timer View";
+    public static String HOURS;
+    public static String MINUTES;
+    public static String SECONDS;
+
+    public RunningTimerViewModel(String viewName) {
+        super(viewName);
+    }
+
+    public static void setHOURS(String HOURS) {
+        RunningTimerViewModel.HOURS = HOURS;
+    }
+
+    public static void setMINUTES(String MINUTES) {
+        RunningTimerViewModel.MINUTES = MINUTES;
+    }
+
+    public static void setSECONDS(String SECONDS) {
+        RunningTimerViewModel.SECONDS = SECONDS;
+    }
+}

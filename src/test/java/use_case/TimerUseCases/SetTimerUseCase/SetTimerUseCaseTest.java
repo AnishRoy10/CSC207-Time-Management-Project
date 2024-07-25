9e5cb00a13c89bfc1e606bd0a2365b69eba19f49
Anish Roy <anishroy991@gmail.com>
2024-07-25 18:24:38 -0400

    added a test file for the set timer use case

--- src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
+++ src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
@@ -1,26 +1,66 @@
 package use_case.TimerUseCases.SetTimerUseCase;
 
+import data_access.FileCacheUserDataAccessObject;
+import data_access.TimerDataAccessObject;
+import entity.Course;
+import entity.User;
+import interface_adapter.presenter.TimerPresenter;
+import interface_adapter.viewmodel.RunningTimerViewModel;
+import interface_adapter.viewmodel.SetTimerViewModel;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import repositories.UserRepository;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class SetTimerUseCaseTest {
-    private SetTimerDataAccessInterface setTimerDataAccessInterface;
-    private SetTimerOutputBoundary setTimerOutputBoundary;
-    private SetTimerInteractor setTimerInteractor;
+    private FileCacheUserDataAccessObject fileCacheUserDAO;
+    private final String testFilePath = "test_userCache.json";
+    private SetTimerDataAccessInterface userRepository;
+//    private SetTimerOutputBoundary setTimerOutputBoundary;
+//    private SetTimerInteractor setTimerInteractor;
 
     @BeforeEach
-    public void setUp() {
-        setTimerDataAccessInterface = mock(SetTimerDataAccessInterface.class);
-        setTimerOutputBoundary = mock(SetTimerOutputBoundary.class);
-        setTimerInteractor = new SetTimerInteractor(setTimerDataAccessInterface, setTimerOutputBoundary);
+    public void setUp() throws IOException {
+        fileCacheUserDAO = new FileCacheUserDataAccessObject(testFilePath);
+        userRepository = new TimerDataAccessObject(fileCacheUserDAO);
+
+//        setTimerDataAccessInterface = mock(SetTimerDataAccessInterface.class);
+//        setTimerOutputBoundary = mock(SetTimerOutputBoundary.class);
+//        setTimerInteractor = new SetTimerInteractor(setTimerDataAccessInterface, setTimerOutputBoundary);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        File testFile = new File(testFilePath);
+        if (testFile.exists()) {
+            testFile.delete();
+        }
     }
 
     @Test
-    public void testSetTimerSuccess() {
+    void testSetTimerSuccess() {
         // Test successful initialization of the timer.
+        User user = new User("testUser", "password", new User[]{}, new Course[]{});
+        assertDoesNotThrow(() -> {
+            fileCacheUserDAO.WriteToCache(user);
+            SetTimerViewModel setTimerViewModel = new SetTimerViewModel("set timer");
+            RunningTimerViewModel runningTimerViewModel = new RunningTimerViewModel("running timer");
+            TimerPresenter presenter = new TimerPresenter(setTimerViewModel, runningTimerViewModel);
+            SetTimerInteractor interactor = new SetTimerInteractor(userRepository, presenter);
+
+            SetTimerInputData input = new SetTimerInputData(1, 0, 0);
+
+            interactor.execute(input);
+
+
+        });
     }
 
     @Test

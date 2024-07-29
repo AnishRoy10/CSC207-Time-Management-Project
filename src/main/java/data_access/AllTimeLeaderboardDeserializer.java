5535b4fea749c09c8e07e5976168c9e4fbe73c6d
skyy <jiayinglee0@gmail.com>
2024-07-29 01:57:07 -0400

    - added LocalDateSerializer and Deserializer for all leaderboard classes

---
+++ src/main/java/data_access/AllTimeLeaderboardDeserializer.java
@@ -0,0 +1,24 @@
+package data_access;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import entity.AllTimeLeaderboard;
+import entity.Leaderboard;
+
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class AllTimeLeaderboardDeserializer implements JsonDeserializer<AllTimeLeaderboard> {
+
+    @Override
+    public AllTimeLeaderboard deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String name = jsonObject.get("name").getAsString();
+        Map<String, Integer> scores = context.deserialize(jsonObject.get("scores"), new TypeToken<Map<String, Integer>>() {}.getType());
+        AllTimeLeaderboard leaderboard = new AllTimeLeaderboard(name);
+        for (Map.Entry<String, Integer> entry : scores.entrySet()) {
+            leaderboard.addScore(entry.getKey(), entry.getValue());
+        }
+        return leaderboard;
+    }
+}

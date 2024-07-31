13475b7a7dcf4cacfd128b4cf564c5697f9c0090
Anish Roy <anishroy991@gmail.com>
2024-07-30 22:39:15 -0400

    removed unused imports

--- src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
+++ src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
@@ -11,7 +11,6 @@ import interface_adapter.viewmodel.SetTimerViewModel;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import repositories.UserRepository;
 import use_case.TimerUseCases.PauseTimerUseCase.PauseTimerInteractor;
 import use_case.TimerUseCases.UpdateTimerUseCase.UpdateTimerInteractor;
 
@@ -19,7 +18,6 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 /**
  * End-to-end tests for set timer use case.

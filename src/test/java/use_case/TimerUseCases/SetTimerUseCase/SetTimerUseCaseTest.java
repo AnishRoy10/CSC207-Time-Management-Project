0189adc33bff9fca5d7273e0367a2ef3952d53f2
Anish Roy <anishroy991@gmail.com>
2024-07-30 22:16:25 -0400

    set timer end-to-end test

--- src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
+++ src/test/java/use_case/TimerUseCases/SetTimerUseCase/SetTimerUseCaseTest.java
@@ -4,6 +4,7 @@ import data_access.FileCacheUserDataAccessObject;
 import data_access.TimerDataAccessObject;
 import entity.Course;
 import entity.User;
+import interface_adapter.controller.TimerController;
 import interface_adapter.presenter.TimerPresenter;
 import interface_adapter.viewmodel.RunningTimerViewModel;
 import interface_adapter.viewmodel.SetTimerViewModel;
@@ -11,29 +12,28 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repositories.UserRepository;
+import use_case.TimerUseCases.PauseTimerUseCase.PauseTimerInteractor;
+import use_case.TimerUseCases.UpdateTimerUseCase.UpdateTimerInteractor;
 
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+/**
+ * End-to-end tests for set timer use case.
+ */
 public class SetTimerUseCaseTest {
     private FileCacheUserDataAccessObject fileCacheUserDAO;
     private final String testFilePath = "test_userCache.json";
-    private SetTimerDataAccessInterface userRepository;
-//    private SetTimerOutputBoundary setTimerOutputBoundary;
-//    private SetTimerInteractor setTimerInteractor;
+    private TimerDataAccessObject userRepository;
 
     @BeforeEach
     public void setUp() throws IOException {
         fileCacheUserDAO = new FileCacheUserDataAccessObject(testFilePath);
         userRepository = new TimerDataAccessObject(fileCacheUserDAO);
 
-//        setTimerDataAccessInterface = mock(SetTimerDataAccessInterface.class);
-//        setTimerOutputBoundary = mock(SetTimerOutputBoundary.class);
-//        setTimerInteractor = new SetTimerInteractor(setTimerDataAccessInterface, setTimerOutputBoundary);
     }
 
     @AfterEach
@@ -54,17 +54,76 @@ public class SetTimerUseCaseTest {
             RunningTimerViewModel runningTimerViewModel = new RunningTimerViewModel("running timer");
             TimerPresenter presenter = new TimerPresenter(setTimerViewModel, runningTimerViewModel);
             SetTimerInteractor interactor = new SetTimerInteractor(userRepository, presenter);
+            UpdateTimerInteractor updateTimerInteractor = new UpdateTimerInteractor(userRepository, presenter);
+            PauseTimerInteractor pauseTimerInteractor = new PauseTimerInteractor(userRepository, presenter);
+            TimerController controller = new TimerController(interactor, updateTimerInteractor, pauseTimerInteractor);
+            controller.execute_set_timer("1", "0", "0");
 
-            SetTimerInputData input = new SetTimerInputData(1, 0, 0);
-
-            interactor.execute(input);
-
-
+            assertEquals("1", RunningTimerViewModel.HOURS);
+            assertEquals("0", RunningTimerViewModel.MINUTES);
+            assertEquals("0", RunningTimerViewModel.SECONDS);
         });
     }
 
     @Test
     public void testSetTimerZeroInput() {
         // Test when inputs are all zero.
+        User user = new User("testUser", "password", new User[]{}, new Course[]{});
+        assertDoesNotThrow(() -> {
+            fileCacheUserDAO.WriteToCache(user);
+            SetTimerViewModel setTimerViewModel = new SetTimerViewModel("set timer");
+            RunningTimerViewModel runningTimerViewModel = new RunningTimerViewModel("running timer");
+            TimerPresenter presenter = new TimerPresenter(setTimerViewModel, runningTimerViewModel);
+            SetTimerInteractor interactor = new SetTimerInteractor(userRepository, presenter);
+            UpdateTimerInteractor updateTimerInteractor = new UpdateTimerInteractor(userRepository, presenter);
+            PauseTimerInteractor pauseTimerInteractor = new PauseTimerInteractor(userRepository, presenter);
+            TimerController controller = new TimerController(interactor, updateTimerInteractor, pauseTimerInteractor);
+            controller.execute_set_timer("0", "0", "0");
+
+            assertNull(RunningTimerViewModel.HOURS);
+            assertNull(RunningTimerViewModel.MINUTES);
+            assertNull(RunningTimerViewModel.SECONDS);
+        });
+    }
+
+    @Test
+    public void testSetTimerOverSixtyInput(){
+        User user = new User("testUser", "password", new User[]{}, new Course[]{});
+        assertDoesNotThrow(() -> {
+            fileCacheUserDAO.WriteToCache(user);
+            SetTimerViewModel setTimerViewModel = new SetTimerViewModel("set timer");
+            RunningTimerViewModel runningTimerViewModel = new RunningTimerViewModel("running timer");
+            TimerPresenter presenter = new TimerPresenter(setTimerViewModel, runningTimerViewModel);
+            SetTimerInteractor interactor = new SetTimerInteractor(userRepository, presenter);
+            UpdateTimerInteractor updateTimerInteractor = new UpdateTimerInteractor(userRepository, presenter);
+            PauseTimerInteractor pauseTimerInteractor = new PauseTimerInteractor(userRepository, presenter);
+            TimerController controller = new TimerController(interactor, updateTimerInteractor, pauseTimerInteractor);
+            controller.execute_set_timer("1", "61", "0");
+
+            assertEquals("2", RunningTimerViewModel.HOURS);
+            assertEquals("1", RunningTimerViewModel.MINUTES);
+            assertEquals("0", RunningTimerViewModel.SECONDS);
+        });
+    }
+
+    @Test
+    public void testSetTimerNonNumericInput() {
+        // Test when inputs are all zero.
+        User user = new User("testUser", "password", new User[]{}, new Course[]{});
+        assertDoesNotThrow(() -> {
+            fileCacheUserDAO.WriteToCache(user);
+            SetTimerViewModel setTimerViewModel = new SetTimerViewModel("set timer");
+            RunningTimerViewModel runningTimerViewModel = new RunningTimerViewModel("running timer");
+            TimerPresenter presenter = new TimerPresenter(setTimerViewModel, runningTimerViewModel);
+            SetTimerInteractor interactor = new SetTimerInteractor(userRepository, presenter);
+            UpdateTimerInteractor updateTimerInteractor = new UpdateTimerInteractor(userRepository, presenter);
+            PauseTimerInteractor pauseTimerInteractor = new PauseTimerInteractor(userRepository, presenter);
+            TimerController controller = new TimerController(interactor, updateTimerInteractor, pauseTimerInteractor);
+            controller.execute_set_timer("a", "0", "1");
+
+            assertNull(RunningTimerViewModel.HOURS);
+            assertNull(RunningTimerViewModel.MINUTES);
+            assertNull(RunningTimerViewModel.SECONDS);
+        });
     }
 }
